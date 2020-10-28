67d5e34814f1d0b4aa23795150034f1481111145
shimon3104 <shimon.aida@gmail.com>
2020-10-28 15:33:26 +0900

    新規登録・ログイン・ログアウト実装
    バリデーション
    CSSファイル導入
    ビューファイルのパス設定
    ログイン/ログアウト時の表示切り替え

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,       presence: true
+  validates :first_name_ruby,       presence: true
+  validates :last_name_ruby,       presence: true
+  validates :birthday, presence: true
+   
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name_ruby
+    validates :last_name_ruby
+  end
+
 end
