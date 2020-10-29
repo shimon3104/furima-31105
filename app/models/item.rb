b2d2b59e13b0460241ad78755e59a074ce7b55ef
shimon3104 <shimon.aida@gmail.com>
2020-10-29 15:25:18 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_charges_id
+    validates :shipping_area_id
+    validates :days_to_ship_id
+    validates :price
+  end
+  
 end
