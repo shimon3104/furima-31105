e4c7654448f4560113c3a3608bf1c1846f552e02
shimon3104 <shimon.aida@gmail.com>
2020-10-29 15:36:23 +0900

    Active_Storage関連導入
    mini_magick
    image_processing
    マイグレート
    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :description
     validates :category_id
