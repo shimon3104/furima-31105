fbae22929bb4e00e008d1ef8e258be430c40016a
shimon3104 <shimon.aida@gmail.com>
2020-10-29 19:09:33 +0900

    DaysToShipモデル、バリデーション整理

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,24 +4,23 @@ class Item < ApplicationRecord
   belongs_to_active_hash :item_status
   belongs_to_active_hash :shipping_charge
   belongs_to_active_hash :shipping_area
-  belongs_to :user
+  belongs_to_active_hash :days_to_ship
   has_one_attached :image
 
   with_options presence: true do
     validates :image
     validates :name
     validates :description
+  end
+  
+  with_options presence: true, numericality: { other_than: 0 } do
     validates :category_id
     validates :item_status_id
     validates :shipping_charges_id
     validates :shipping_area_id
     validates :days_to_ship_id
-    validates :category
   end
-  
+
   validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'Include both numbers' }
-  validates :category_id, numericality: { other_than: 0 }
-  validates :item_status_id, numericality: { other_than: 0 }
-  validates :shipping_charges_id, numericality: { other_than: 0 }
-  validates :shipping_area_id, numericality: { other_than: 0 }
+  
 end
