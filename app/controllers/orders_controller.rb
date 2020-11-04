f1894848a7efe34c3cca187a6178b89ca70e5365
shimon3104 <shimon.aida@gmail.com>
2020-11-04 16:25:58 +0900

    formオブジェクト

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,22 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order_receive = OrderReceive.new
+  end
+
+  def create
+    @order_receive = OrderReceive.new(receive_params)
+    if @order_receive.valid?
+      @order_receive.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def receive_params
+    params.require(:order_receive).permit(:postal_code, :prefecture_id, :city_name, :address, :phone_number).merge(user_id: current_user.id, item_id: params[item.id])
   end
 end
--- app/models/order_receive.rb
+++ app/models/order_receive.rb
@@ -9,4 +9,9 @@ class OrderReceive
     validates :address
     validates :phone_number, format: { with: /\A[0-9]+\z/, message: "Input only number" }
   end
+
+  def save
+    order = Order.create(user_id: current_user.id, item_id: params[item.id])
+    Receive.create(postal_code: postal_code, presence: presence, city_name: city_name, address: address, phone_number: phone_number, order_id: order.id)
+  end
 end
